996d2df78b4894eb99c0e977faa298438d5ae6c0
RuDr8A <rudrasingh240310@gmail.com>
2026-08-16 00:08:18 +0530

    Create the random attack generator, which will creat a thread, which will to the frontend

--- dos-map-backend/server.js
+++ dos-map-backend/server.js
@@ -1,19 +1,49 @@
 
 require('dotenv').config();
+const http = require('http');
+const { Server } = require('socket.io');
 const app = require("./src/app");
 const connectToDB = require('./src/database/db');
+const mockGenerator = require('./src/utils/mockDataGenerator');
 
 const PORT = process.env.PORT || 3000; 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*", 
+        methods: ["GET", "POST"]
+    }
+});
 
 async function startServer() {
   try {
     
     await connectToDB();
-    
-    
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}....`);
     });
+
+    io.on("connection", (socket) => {
+        console.log(` New client connected: ${socket.id}`);
+
+        socket.on("disconnect", () => {
+            console.log(` Client disconnected: ${socket.id}`);
+        });
+    });
+
+    
+    setInterval(() => {
+        
+        if (io.sockets.sockets.size > 0) {
+            const newAttack = mockGenerator();
+            io.emit("new-attack", newAttack); 
+            console.log(`Broadcasting attack to ${io.sockets.sockets.size} clients...`, newAttack.threatLevel);
+        } else {
+            console.log("Stream idle: No clients connected.");
+        }
+    }, 2000);
+
   } catch (error) {
     console.error("Critical: Server failed to start:", error.message);
     process.exit(1);
---
+++ dos-map-backend/src/utils/mockDataGenerator.js
@@ -0,0 +1,24 @@
+function mockGenerator() {
+    
+    // Source and target latitude 
+    const sourceLat = Number((Math.random() * 180 - 90).toFixed(4));
+    const targetLat = Number((Math.random() * 180 - 90).toFixed(4));
+
+    // Source and target longitude 
+    const sourceLng = Number((Math.random() * 360 - 180).toFixed(4));
+    const targetLng = Number((Math.random() * 360 - 180).toFixed(4));
+
+    const threatLevels = ['LOW', 'MEDIUM', 'HIGH', 'CRITICAL'];
+    const threatLevelIndex = Math.floor(Math.random() * threatLevels.length);
+
+    return {
+        "sourceLat": sourceLat,
+        "sourceLng": sourceLng,
+        "targetLat": targetLat,
+        "targetLng": targetLng,
+        "threatLevel": threatLevels[threatLevelIndex]
+    };
+}
+
+
+module.exports = mockGenerator;
